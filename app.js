4deac5d97fecc6ebf9bd55edcfe8a756782f9225
ErickDavidSoto <edsr2092.us@gmail.com>
2023-06-03 19:07:28 -0300

    webserver

---
+++ app.js
@@ -0,0 +1,41 @@
+
+const express = require('express')
+var hbs = require('hbs');
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT;
+
+
+
+//HBS PARA RENDERIZAR UNA VISTA
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+//Servir contenido estatico 
+app.use( express.static('Public'));
+
+app.get('/', function (req, res) {
+    res.render('home',{
+        titulo:"CURSO DE NODE",
+        nombre:"Erick David"
+    });
+});
+
+app.get('/generic', function (req, res) {
+    res.render('generic');
+});
+
+app.get('/elements', function (req, res) {
+    res.render('elements');
+});
+
+app.get('*', function (req, res) {
+res.sendFile(__dirname + '/public/404.html')
+});
+
+  
+app.listen(port, () => {
+    console.log(`example app listening at htt_localhost:${port}`)
+})
